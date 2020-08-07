355107ddc2373806fc8ef1e073da5449373483ba
psiha <psihachina@gmail.com>
2020-08-07 12:38:42 +0300

    Added Login route

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -2,11 +2,16 @@ package apiserver
 
 import (
 	"encoding/json"
+	"errors"
 	"github.com/gorilla/mux"
 	"github.com/psihachina/go-test-work.git/internal/app/model"
 	"github.com/psihachina/go-test-work.git/internal/app/store"
 	"github.com/sirupsen/logrus"
 	"net/http"
+)
+
+var (
+	errIncorrectEmailOrPassword = errors.New("incorrect email or password")
 )
 
 type server struct {
@@ -33,6 +38,7 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (s *server) configureRouter() {
 	s.router.HandleFunc("/Register", s.handleUsersCreate()).Methods("POST")
+	s.router.HandleFunc("/Login", s.handleSessionsCreate()).Methods("POST")
 }
 
 func (s *server) handleUsersCreate() http.HandlerFunc {
@@ -44,7 +50,7 @@ func (s *server) handleUsersCreate() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		req := &request{}
 		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
-			s.error(w, http.StatusBadRequest, err)
+			s.error(w, r, http.StatusBadRequest, err)
 			return
 		}
 		u := &model.User{
@@ -52,19 +58,41 @@ func (s *server) handleUsersCreate() http.HandlerFunc {
 			Password: req.Password,
 		}
 		if err := s.store.User().Create(u); err != nil {
-			s.error(w, http.StatusUnprocessableEntity, err)
+			s.error(w, r, http.StatusUnprocessableEntity, err)
 			return
 		}
-		u.Sanitaize()
-		s.respond(w, http.StatusCreated, u)
+		u.Sanitize()
+		s.respond(w, r, http.StatusCreated, u)
 	}
 }
 
-func (s *server) error(w http.ResponseWriter, code int, err error) {
-	s.respond(w, code, map[string]string{"error": err.Error()})
+func (s *server) handleSessionsCreate() http.HandlerFunc {
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, r, http.StatusBadRequest, err)
+			return
+		}
+
+		u, err := s.store.User().FindByEmail(req.Email)
+		if err != nil || !u.ComparePassword(req.Password) {
+			s.error(w, r, http.StatusUnauthorized, errIncorrectEmailOrPassword)
+		}
+
+		s.respond(w, r, http.StatusOK, nil)
+	}
 }
 
-func (s *server) respond(w http.ResponseWriter, code int, data interface{}) {
+func (s *server) error(w http.ResponseWriter, r *http.Request, code int, err error) {
+	s.respond(w, r, code, map[string]string{"error": err.Error()})
+}
+
+func (s *server) respond(w http.ResponseWriter, r *http.Request, code int, data interface{}) {
 	w.WriteHeader(code)
 	if data != nil {
 		_ = json.NewEncoder(w).Encode(data)
