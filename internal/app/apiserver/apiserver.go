22764d1b6a007bf784ed43b6e7882e09ee33f2a3
psiha <psihachina@gmail.com>
2020-08-07 10:08:25 +0300

    Refactored app module and added server.go

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,78 +1,42 @@
 package apiserver
 
 import (
-	"github.com/gorilla/mux"
-	"github.com/psihachina/go-test-work.git/internal/app/store"
-	"github.com/sirupsen/logrus"
-	"io"
+	"context"
+	"github.com/psihachina/go-test-work.git/internal/app/store/mongodbstore"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
 	"net/http"
+	"time"
 )
 
-// APIServer ...
-type APIServer struct {
-	config *Config
-	logger *logrus.Logger
-	router *mux.Router
-	store  *store.Store
-}
-
-// New ...
-func New(config *Config) *APIServer {
-	return &APIServer{
-		config: config,
-		logger: logrus.New(),
-		router: mux.NewRouter(),
-	}
-}
-
-// Start ...
-func (s *APIServer) Start() error {
-	if err := s.configureLogger(); err != nil {
-		return err
-	}
-
-	s.configureRouter()
-
-	if err := s.configureStore(); err != nil {
-		return err
-	}
-
-	s.logger.Info("starting api server")
-
-	return http.ListenAndServe(s.config.BindAddr, s.router)
-}
-
-func (s *APIServer) configureLogger() error {
-	level, err := logrus.ParseLevel(s.config.LogLevel)
-
+func Start(config *Config) error {
+	db, err := newDB(config.DatabaseUrl)
 	if err != nil {
 		return err
 	}
 
-	s.logger.SetLevel(level)
+	defer db.Client().Disconnect(context.Background())
 
-	return nil
+	store := mongodbstore.New(db)
+
+	srv := newServer(store)
+
+	return http.ListenAndServe(config.BindAddr, srv)
 }
 
-func (s *APIServer) configureStore() error {
-	st := store.New(s.config.Store)
-	if err := st.Open("test_work"); err != nil {
-		return err
+func newDB(databaseUrl string) (*mongo.Database, error) {
+	db, err := mongo.NewClient(options.Client().ApplyURI(databaseUrl))
+	if err != nil {
+		return nil, err
 	}
 
-	s.store = st
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
 
-	return nil
-}
-
-func (s *APIServer) configureRouter() {
-	s.router.HandleFunc("/hello", s.handleHello())
-}
-
-func (s *APIServer) handleHello() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		if _, err := io.WriteString(w, "Hello"); err != nil {
-			s.logger.Fatal(err)
-		}
+	err = db.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	return db.Database("test_work"), nil
 }
---
+++ internal/app/apiserver/server.go
@@ -0,0 +1,72 @@
+package apiserver
+
+import (
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/psihachina/go-test-work.git/internal/app/model"
+	"github.com/psihachina/go-test-work.git/internal/app/store"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+type server struct {
+	router *mux.Router
+	logger *logrus.Logger
+	store  store.Store
+}
+
+func newServer(store store.Store) *server {
+	s := &server{
+		router: mux.NewRouter(),
+		logger: logrus.New(),
+		store:  store,
+	}
+
+	s.configureRouter()
+
+	return s
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.router.ServeHTTP(w, r)
+}
+
+func (s *server) configureRouter() {
+	s.router.HandleFunc("/Register", s.handleUsersCreate()).Methods("POST")
+}
+
+func (s *server) handleUsersCreate() http.HandlerFunc {
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := &request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			s.error(w, http.StatusBadRequest, err)
+			return
+		}
+		u := &model.User{
+			Email:    req.Email,
+			Password: req.Password,
+		}
+		if err := s.store.User().Create(u); err != nil {
+			s.error(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+		u.Sanitaize()
+		s.respond(w, http.StatusCreated, u)
+	}
+}
+
+func (s *server) error(w http.ResponseWriter, code int, err error) {
+	s.respond(w, code, map[string]string{"error": err.Error()})
+}
+
+func (s *server) respond(w http.ResponseWriter, code int, data interface{}) {
+	w.WriteHeader(code)
+	if data != nil {
+		_ = json.NewEncoder(w).Encode(data)
+	}
+}
